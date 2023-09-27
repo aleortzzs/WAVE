5226d0be0f4694b317152af14bd2c42758be1f9d
M034792 JOSE MARTINEZ <jose.martinez@cetysuniversidad.edu.mx>
2023-09-26 20:14:24 -0700

    Registro de usuario completo
    es posible registrar todos los datos de usuario POST
    y actualizarlos PUT para añadir
    las descripciones
    
    Falta el login

--- src/app/components/create-profile/create-profile.component.ts
+++ src/app/components/create-profile/create-profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, AfterViewInit,Input } from '@angular/core';
+import { GenreService } from 'src/app/Servicios/genre-service.service';
 import { UsuariosService } from 'src/app/Servicios/usuarios.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-profile',
   templateUrl: './create-profile.component.html',
@@ -8,21 +11,31 @@ import { UsuariosService } from 'src/app/Servicios/usuarios.service';
 export class CreateProfileComponent implements AfterViewInit {
   cuadros: any[] = []; // Variable de matriz para almacenar los cuadros adicionales
   prevData: any;
+  user: any;
+  userId: any;
   @Input() data: any={
     short :'',
     descripcion:'',
     precio:'',
     collab:'',
   };
-  constructor(public usuarioService: UsuariosService){}
+  constructor(public usuarioService: UsuariosService, 
+    public genreService: GenreService, 
+    private route: ActivatedRoute,
+    public router: Router){}
   agregarCuadro() {
     this.cuadros.push({}); // Agregar un nuevo cuadro vacío a la matriz
   }
 
   ngOnInit(): void{
     this.prevData = JSON.parse(localStorage.getItem('signupData') || '{}')
+    this.route.params.subscribe(params => {
+      this.userId = params['id'];
+    });
   }
- 
+  getGenreName(genero: string){
+    return this.genreService.getGenreName(genero)
+  }
 
   selectedImage: string | ArrayBuffer | null = null;
 
@@ -48,8 +61,33 @@ export class CreateProfileComponent implements AfterViewInit {
   }
   session_localStorage(){
     localStorage.setItem('signupData',JSON.stringify(this.combineData()));
+    this.user = JSON.parse(localStorage.getItem('signupData') || '{}');
+    console.log(this.user)
+    this.updateUserById();
  }
  
+ updateUserById(){
+    if(!this.userId){
+      console.log('User ID is not availabe.');
+      return;
+    }
+
+    this.usuarioService.updateUsuariosById(this.userId, this.user)
+      .subscribe(
+        response => {
+          console.log("user updated succesfully", response);
+        },
+        error =>{
+          console.error("Error updating user:", error);
+        }
+      );
+    localStorage.clear()
+    this.usuarioService.getUsuarioById(this.userId).subscribe(user => {
+      this.router.navigate(['/profile', this.userId], { state: { user } });
+      console.log(user);
+    });
+ }
+
  combineData(){
   const combineData={
     ...this.prevData,
--- src/app/components/signup-musico/signup-musico.component.ts
+++ src/app/components/signup-musico/signup-musico.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit,Input, ViewChild, ComponentRef, ViewCo
 import { UsuariosService } from 'src/app/Servicios/usuarios.service';
 import { GenerosCardComponent } from '../generos-card/generos-card.component';
 import { DynamicComponentDirective } from 'src/app/dynamic-component.directive';
+import { Router } from '@angular/router';
 
 
 
@@ -19,18 +20,30 @@ export class SignupMusicoComponent implements OnInit {
   @Input() data:any={
   nombre:   '',
   apellido :   '',
-  pais:   '',
+  localizacion:   '',
   fecha:   '',
+  userId: ''
   }
 
-  constructor(public usuarioService : UsuariosService){
+  constructor(public usuarioService : UsuariosService, private router: Router){
   }
 
 handleClick(){
   this.prevData = JSON.parse(localStorage.getItem('signupData') || '{}')
-  localStorage.setItem('signupData', JSON.stringify(this.combineData()));
-  this.usuarioService.createUsuario(JSON.parse(localStorage.getItem('signupData')||'{}'));
+  this.usuarioService.createUsuario(JSON.parse(localStorage.getItem('signupData')|| '{}'))
+  .subscribe(
+    (createdUser: any) =>{
+      this.data.userId = createdUser._id
+      localStorage.setItem('signupData', JSON.stringify(this.combineData()));
+      this.router.navigate(['create-profile', createdUser._id])
+      localStorage.setItem('userId', createdUser._id);
+    },
+    (error) => {
+      console.error("There was an error creating the user: ", error);
+    }
+  );
 }
+
  ngOnInit(): void {
    
  }
